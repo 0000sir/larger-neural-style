b40ee02467f7b4b7beae3e0133e3f5a61722bde5
ProGamerGov <ProGamerGov@users.noreply.github.com>
2016-11-04 18:32:17 -0600

    Fixed tile alignment

--- bigbrush.sh
+++ bigbrush.sh
@@ -38,11 +38,10 @@ main(){
 	
 	w2=`convert $out_dir/$clean_name'_0.png' -format "%w" info:`
 	h2=`convert $out_dir/$clean_name'_0.png' -format "%h" info:`
-	w_percent=`echo 20 $w2 | awk '{print $1/$2}'`
-	h_percent=`echo 20 $h2 | awk '{print $1/$2}'`
+	#w_percent=`echo 20 $w2 | awk '{print $1/$2}'`
+	#h_percent=`echo 20 $h2 | awk '{print $1/$2}'`
 
-	border_w=`echo $w1 $w_percent | awk '{print $1*$2}'`
-	border_h=`echo $h1 $h_percent | awk '{print $1*$2}'`
+	
 	
 	#Resize all tiles to avoid ImageMagick weirdness
 	 convert $out_dir/$clean_name'_0.png' -resize "$w2"x"$h2"\! $out_dir/$clean_name'_0.png'
@@ -53,9 +52,7 @@ main(){
 	 convert $out_dir/$clean_name'_5.png' -resize "$w2"x"$h2"\! $out_dir/$clean_name'_5.png'
 	 convert $out_dir/$clean_name'_6.png' -resize "$w2"x"$h2"\! $out_dir/$clean_name'_6.png'
 	 convert $out_dir/$clean_name'_7.png' -resize "$w2"x"$h2"\! $out_dir/$clean_name'_7.png'
-	 convert $out_dir/$clean_name'_8.png' -resize "$w2"x"$h2"\! $out_dir/$clean_name'_8.png'
-
-	 					
+	 convert $out_dir/$clean_name'_8.png' -resize "$w2"x"$h2"\! $out_dir/$clean_name'_8.png'	 					
 					#WxH			
 
 	# 4. neural-style each tile
@@ -69,6 +66,39 @@ main(){
 		neural_style_tiled $out_dir/$tile $style $tiles_dir/$tile
 	done
 	
+	#Test	
+
+
+
+	w2_2=`convert $tiles_dir/$clean_name'_0.png' -format "%w" info:`
+	h2_2=`convert $tiles_dir/$clean_name'_0.png' -format "%h" info:`
+	w_20=20
+	h_20=20
+
+	#w2_22="$w2"/"$w2_2"
+	#h2_22="$h2"/"$h2_2"
+
+	w2_22=`echo $w2 $w2_2 | awk '{print $1/$2}'`
+	h2_22=`echo $h2 $h2_2 | awk '{print $1/$2}'`
+
+
+	border_w_2=`echo $w2_22 $w_20 | awk '{print $1*$2}'`
+	border_h_2=`echo $h2_22 $h_20 | awk '{print $1*$2}'`
+	
+
+	border_w=`echo $border_w_2 $w_20 | awk '{print $1+$2}'`
+	border_h=`echo $border_h_2 $h_20 | awk '{print $1+$2}'`
+
+	#border_w=`echo $w1 $w_percent | awk '{print $1*$2}'`
+	#border_h=`echo $h1 $h_percent | awk '{print $1*$2}'`
+
+	
+	
+	w_percent=`echo 20 $w2_2 | awk '{print $1/$2}'`
+	h_percent=`echo 20 $h2_2 | awk '{print $1/$2}'`
+
+	#Test
+
 	# 5. feather tiles
 	feathered_dir=$out_dir/feathered
 	mkdir -p $feathered_dir
@@ -84,7 +114,7 @@ main(){
 					$feathered_dir/$clean_name'_4.png' $feathered_dir/$clean_name'_5.png' \
 					$feathered_dir/$clean_name'_6.png' $feathered_dir/$clean_name'_7.png' \
 					$feathered_dir/$clean_name'_8.png'  -tile 3x3 -geometry -$border_w-$border_h $output/$clean_name.large_feathered.png
-
+ # -$border_w-$border_h
 
 	# 7. merge un-feathered tiles
 	montage $tiles_dir/$clean_name'_0.png' $tiles_dir/$clean_name'_1.png' \
@@ -107,8 +137,9 @@ neural_style(){
 	if [ ! -s $3 ]; then
 		th neural_style.lua -content_image $1 -style_image $2 -output_image $3 \
 				-image_size 1000 -print_iter 100 -backend cudnn -gpu 0 -save_iter 0 \
-				-style_weight 20 -num_iterations 500
+				-style_weight 20 -num_iterations 250 
 				#-original_colors 1
+				# A lower iteration count for the tiles results in less divergence between tiles
 				#Change the command including "th neural_style.lua" to use other neural network systems.
 	fi
 	if [ ! -s $3 ] && [ $retry -lt 3 ] ;then
@@ -125,7 +156,7 @@ neural_style_tiled(){
 	echo "Neural Style Transfering "$1
 	if [ ! -s $3 ]; then
 		th neural_style.lua -content_image $1 -style_image $2 -output_image $3 \
-				-image_size 1000 -print_iter 100 -backend cudnn -gpu 0 -save_iter 0 \
+			-image_size 1000 -print_iter 100 -backend cudnn -gpu 0 -save_iter 0 \
 				-style_weight 20 -num_iterations 250 
 				#-original_colors 1
 				# A lower iteration count for the tiles results in less divergence between tiles
